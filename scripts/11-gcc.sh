f648b12de1ba53d4c7561dade65522762585c42f
Zhiguang Wang <wangzhiguang@kingsoft.com>
2018-07-04 16:09:47 +0800

    Add ppa:ubuntu-toolchain-r/test when ubuntu version is not 18.04

--- scripts/11-gcc.sh
+++ scripts/11-gcc.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -ex
 
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+CODENAME=$(lsb_release -s -c)
+
+# Ubuntu 18.04 comes with GCC 7 already
+if [ "$CODENAME" != "bionic" ]; then
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+fi
+
 sudo apt-get update
 sudo apt-get install -y g++-7
